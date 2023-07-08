2821b266dcc7f4f236379760f2037f0b8d84b90b
Maxi-Soria <mfsoria16@gmail.com>
2023-07-08 19:31:24 -0300

    Agregue guia 9 clase de utilidad

--- Guia-1-PooExtra/src/main/java/maxi/guiapooextra/GuiaPooExtra.java
+++ Guia-1-PooExtra/src/main/java/maxi/guiapooextra/GuiaPooExtra.java
@@ -2,11 +2,13 @@ package maxi.guiapooextra;
 
 import maxi.guiapooextra.entidades.Cancion;
 import maxi.guiapooextra.entidades.Cuenta;
+import maxi.guiapooextra.entidades.Empleado;
 import maxi.guiapooextra.entidades.Juego;
 import maxi.guiapooextra.entidades.Puntos;
+import maxi.guiapooextra.entidades.Rectangulo;
 
 public class GuiaPooExtra {
-
+    
     public static void main(String[] args) {
         
         
@@ -30,5 +32,13 @@ public class GuiaPooExtra {
         Cuenta obj = new Cuenta(50000, "Maximiliano Soria");
         System.out.println("El saldo en la cuenta es: " + obj.retirarDinero(10000));   
     }
-    
+    static void punto5(){
+        Empleado obj = new Empleado("Maxi", 28, 50000);
+        obj.calcularAumento();
+        System.out.println(obj.toString());        
+    }
+    static void punyo6(){
+        Rectangulo rectangulo1 = new Rectangulo(4, 6);
+        System.out.println("El area rel rectangulo es " + rectangulo1.calcularArea() + " mts2");
+    }
 }
